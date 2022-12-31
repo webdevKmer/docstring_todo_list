f50e0022840cb79f4b1ac704aba1c4ef46c76ada
webdevKmer <webdevKmer@gmail.com>
2022-12-31 05:33:03 +0000

    create 4 pages and the navbar

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Collection(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=70)
     slug = models.SlugField()
 
+class Task(models.Model):
+    description = models.CharField(max_length=255)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def all_tasks(request):
+    return render(request, 'tasks/tasks.html', {})
+
+def home(request):
+    return render(request, 'tasks/index.html', {})
+
+def about(request):
+    return render(request, 'tasks/about.html', {})
+
+def contact(request):
+    return render(request, 'tasks/contact.html', {})
